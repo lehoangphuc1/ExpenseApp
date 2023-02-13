a66f934458b68fe4558a6d03e7bb461569df6d81
PhuongNC <phuongnc@rikkeisoft.com>
2023-02-13 22:12:11 +0700

    Init source

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <h2>Training ReactJS</h2>
+      </header>
+    </div>
+  );
+};
+
+export default App;
